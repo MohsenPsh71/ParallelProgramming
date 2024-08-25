338c362a419febf67ca19f8a607ba7fd6459776e
mohsen <71mohsenpsh@gmail.com>
2024-08-25 23:49:03 +0330

    using AggregateException

---
+++ AggregateException/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void methode()
+{
+    var t = Task.Factory.StartNew(() =>
+    {
+        throw new DivideByZeroException();
+        //throw new AccessViolationException();
+    });
+    try
+    {
+        t.Wait();
+    }
+    catch (AggregateException Aggregate)
+    {
+        foreach (var item in Aggregate.InnerExceptions)
+        {
+            Console.WriteLine($"Exception {item.GetType()}");
+        }
+
+    }
+}
+methode();
