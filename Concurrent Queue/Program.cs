6d37d0053d971e8e7982384a6a52b2df7aa02f26
mohsen <71mohsenpsh@gmail.com>
2024-08-26 02:46:24 +0330

    ConcurrentStack

--- Concurrent Queue/Program.cs
+++ Concurrent Queue/Program.cs
@@ -15,3 +15,29 @@ if (q.TryPeek(out resault))
 {
     Console.WriteLine($"Last Item {resault}");
 }
+
+//LIFO Last in First Out
+ConcurrentStack<int> ints = new ConcurrentStack<int>();
+ints.Push(1);
+ints.Push(2);
+ints.Push(3);
+ints.Push(4);
+ints.Push(5);
+
+int resault2;
+if (ints.TryPop(out resault2))
+{
+    Console.WriteLine($"I Removed {resault2}");
+}
+if (ints.TryPeek(out resault2))
+{
+    Console.WriteLine($"Last Item {resault2}");
+}
+
+var items = new int[3];
+if (ints.TryPopRange(items,0,3)>0)
+{
+    var text = string.Join(" , ",items.Select(i => i.ToString()));
+    Console.WriteLine($"Deleted Items {text}");
+}
+
