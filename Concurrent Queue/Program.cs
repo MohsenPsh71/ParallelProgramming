2e4fac7a337637fc3414e3a491956405814d7ec4
mohsen <71mohsenpsh@gmail.com>
2024-08-26 02:37:27 +0330

    ConcurrentQueue

---
+++ Concurrent Queue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+
+//FIFO First in First Out
+ConcurrentQueue<int> q = new ConcurrentQueue<int>();
+q.Enqueue(1);
+q.Enqueue(2);
+q.Enqueue(3);
+
+int resault;
+if (q.TryDequeue(out resault))
+{
+    Console.WriteLine($"I Removed {resault}");
+}
+if (q.TryPeek(out resault))
+{
+    Console.WriteLine($"Last Item {resault}");
+}
