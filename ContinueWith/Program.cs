0691e348e378d7621fd859e5b5d6e5d38836c2cf
mohsen <71mohsenpsh@gmail.com>
2024-08-26 03:31:21 +0330

    ContinueWith

---
+++ ContinueWith/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+static void Method()
+{
+    //ContinueWith
+    var task = Task.Factory.StartNew(() =>
+    {
+        Console.WriteLine($"First Step {Task.CurrentId}");
+    });
+    var task2 = task.ContinueWith(t =>
+    {
+        Console.WriteLine($"Final Step {Task.CurrentId} {t.Status}");
+        if (t.IsFaulted)
+        {
+            throw t.Exception.InnerException;
+        }
+    });
+    try
+    {
+        task2.Wait();
+    }
+    catch (AggregateException ae)
+    {
+
+        ae.Handle(e =>
+        {
+            Console.WriteLine("Excrption" + e);
+            return true;
+        });
+    }
+}
+//Method();
