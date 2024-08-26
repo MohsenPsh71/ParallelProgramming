a24b0e533e687b4c302ada31c3da9e0ce15ae710
mohsen <71mohsenpsh@gmail.com>
2024-08-26 04:25:23 +0330

    SemaphoreSlim

---
+++ SemaphoreSlim/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var semaphore = new SemaphoreSlim(2, 10);
+for (int i = 0; i < 20; i++)
+{
+    Task task = Task.Factory.StartNew(() => 
+    {
+        Console.WriteLine($"Enter task {Task.CurrentId}");
+        semaphore.Wait();
+        Console.WriteLine("Proccessing");
+    });
+}
+while (semaphore.CurrentCount <= 2)
+{
+    Console.ReadKey();
+    semaphore.Release(2);
+}
