dbb54f3db1b9460c741829afafd5a3e1f42d2bb3
mohsen <71mohsenpsh@gmail.com>
2024-08-26 03:31:43 +0330

    ContinueWhenAll

--- ContinueWith/Program.cs
+++ ContinueWith/Program.cs
@@ -28,4 +28,33 @@ static void Method()
         });
     }
 }
+static void Method2()
+{
+    //ContinueWith
+    var task = Task.Factory.StartNew(() => "Task 1");
+    var task2 = Task.Factory.StartNew(() => "Task 2");
+    var task3 = Task.Factory.ContinueWhenAll([task, task2], tasks =>
+    {
+        Console.WriteLine("Tasks completed");
+        foreach (var item in tasks)
+        {
+            Console.WriteLine($"\t{item.Status}");
+            Console.WriteLine($"\t{item.Result}");
+        }
+    });
+    try
+    {
+        task3.Wait();
+    }
+    catch (AggregateException ae)
+    {
+
+        ae.Handle(e =>
+        {
+            Console.WriteLine("Excrption" + e);
+            return true;
+        });
+    }
+}
 //Method();
+Method2();
