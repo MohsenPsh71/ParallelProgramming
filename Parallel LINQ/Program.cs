d1e849198de30619d669cd03a4d32324e26870c5
mohsen <71mohsenpsh@gmail.com>
2024-08-30 21:31:51 +0330

    OperationCanceledException

--- Parallel LINQ/Program.cs
+++ Parallel LINQ/Program.cs
@@ -7,8 +7,36 @@ int[] items = Enumerable.Range(0, count).ToArray();
 
 //items.AsParallel().ForAll<int>(a => Console.WriteLine(a));
 
-ParallelQuery<int> items2 = items.AsParallel().AsOrdered().Select(a => a).Where(a => a>=10);
-foreach (int item in items2)
+//ParallelQuery<int> items2 = items.AsParallel().AsOrdered().Select(a => a).Where(a => a>=10);
+//foreach (int item in items2)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+CancellationTokenSource cts = new CancellationTokenSource();
+ParallelQuery<int> items3 = items.AsParallel().WithCancellation(cts.Token).Select(a => {
+    //OperationCanceledException
+    //throw new NotImplementedException();
+    //return items[a - 1];
+    return a;
+});
+
+//OperationCanceledException
+//cts.Cancel();
+
+try
 {
-    Console.WriteLine(item);
+    foreach (int item in items3)
+    {
+        Console.WriteLine(item);
+    }
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("OperationCanceledException");
+}
+catch (AggregateException)
+{
+    Console.WriteLine("AggregateException");
 }
