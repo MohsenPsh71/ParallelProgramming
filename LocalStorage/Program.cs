c69944d37d4577a5d5ee8f2ccb310d4d5fabd106
mohsen <71mohsenpsh@gmail.com>
2024-08-30 20:55:55 +0330

    use Interlocked

---
+++ LocalStorage/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int sum = 0;
+//Parallel.For(0,1001,a => {
+//    Interlocked.Add(ref sum,a);
+//});
+
+// this code faster & have better performance
+Parallel.For(0, 1001,() =>0,(a,state,b) =>
+{
+    b += a;
+    //if (a == 1) {
+    //    state.Stop(); 
+    //}
+    return b;
+}, finall =>
+{
+    Interlocked.Add(ref sum, finall);
+});
+
+
+
+Console.WriteLine(sum);
+
