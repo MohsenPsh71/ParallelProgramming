1b3f959c8f860d7bb628ffbffb7f6c2e0f5b08b6
mohsen <71mohsenpsh@gmail.com>
2024-08-30 21:20:36 +0330

    Parallel LINQ

---
+++ Parallel LINQ/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+int count = 20;
+int[] items = Enumerable.Range(0, count).ToArray();
+
+//items.AsParallel().ForAll<int>(a => Console.WriteLine(a));
+
+ParallelQuery<int> items2 = items.AsParallel().AsOrdered().Select(a => a).Where(a => a>=10);
+foreach (int item in items2)
+{
+    Console.WriteLine(item);
+}
